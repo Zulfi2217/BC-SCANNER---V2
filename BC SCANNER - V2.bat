16f33bab11182fdf2843a6ca45b470bac4fcdf14
Zulfiqar Chowdhury <111815469+Zulfi2217@users.noreply.github.com>
2022-08-24 08:49:44 +0600

    Add files via upload

---
+++ BC SCANNER - V2.bat
@@ -0,0 +1,222 @@
+��
+
+@echo off 
+title Brother's Corporation Scanner - [%date%]
+mode 70,20
+chcp 65001>nul 
+cls
+echo.
+echo.  ▄▄▄· ▄▄▄·  ▐ ▄ ▄▄▄ .▄▄▌      ·▄▄▄▄• ▄▌ ▄▄· ▄ •▄ ▄▄▄ .▄▄▄[0m  
+echo. ▐█ ▄█▐█ ▀█ •█▌▐█▀▄.▀·██•      ▐▄▄·█▪██▌▐█ ▌▪█▌▄▌▪▀▄.▀·▀▄ █·[0m
+echo.  ██▀·▄█▀▀█ ▐█▐▐▌▐▀▀▪▄██▪      ██▪ █▌▐█▌██ ▄▄▐▀▀▄·▐▀▀▪▄▐▀▀▄ [0m
+echo. ▐█▪·•▐█ ▪▐▌██▐█▌▐█▄▄▌▐█▌▐▌    ██▌.▐█▄█▌▐███▌▐█.█▌▐█▄▄▌▐█•█▌[0m
+echo.. ▀    ▀  ▀ ▀▀ █▪ ▀▀▀ .▀▀▀     ▀▀▀  ▀▀▀ ·▀▀▀ ·▀  ▀ ▀▀▀ .▀  ▀[0m
+echo.
+echo                                               [102m[92m-[0m[0m[43m[33m-[0m[0m[97m - Bangladesh
+echo.
+echo.
+echo [90m      Welcome to [35m[[4mBrother's Corporation Scanner[0m[0m[90m Thanks you for use.
+echo [90m     Who have doubted [33m[4Please contact support.[0m[0m[90m
+echo.
+echo.
+echo [97m     [ 1 ] [95mEquipe[0m[97m                 [ 3 ] [95mSair[0m[ 
+echo [97m     [ 2 ] [95mSuporte[0m[97m                             [ 4 ] [95mEventvwr[0m[ 
+echo [97m     [ 5 ] [95mUsnJournalCheck[0m[97m                           [ 6 ] [95mReg Scanner[0m[ 
+echo.
+echo.
+
+set /p slct=[95m     ►  [0m
+if %slct%==1 goto Equipe
+if %slct%==2 goto Suporte
+if %slct%==3 goto nada
+if %slct%==4 goto event
+if %slct%==5 goto usnjoun
+if %slct%==6 goto regscan
+
+
+
+:Equipe
+@echo off 
+title Brother's Corporation Scanner - [%date%]
+mode 70,20
+chcp 65001>nul 
+cls
+echo.
+echo.  ▄▄▄· ▄▄▄·  ▐ ▄ ▄▄▄ .▄▄▌      ·▄▄▄▄• ▄▌ ▄▄· ▄ •▄ ▄▄▄ .▄▄▄[0m  
+echo. ▐█ ▄█▐█ ▀█ •█▌▐█▀▄.▀·██•      ▐▄▄·█▪██▌▐█ ▌▪█▌▄▌▪▀▄.▀·▀▄ █·[0m
+echo.  ██▀·▄█▀▀█ ▐█▐▐▌▐▀▀▪▄██▪      ██▪ █▌▐█▌██ ▄▄▐▀▀▄·▐▀▀▪▄▐▀▀▄ [0m
+echo. ▐█▪·•▐█ ▪▐▌██▐█▌▐█▄▄▌▐█▌▐▌    ██▌.▐█▄█▌▐███▌▐█.█▌▐█▄▄▌▐█•█▌[0m
+echo.. ▀    ▀  ▀ ▀▀ █▪ ▀▀▀ .▀▀▀     ▀▀▀  ▀▀▀ ·▀▀▀ ·▀  ▀ ▀▀▀ .▀  ▀[0m
+echo.
+echo.
+echo [90m   [4mDeveloper[0m[0m[90m          [4mString Finders[0m[0m[90m 
+echo.
+echo [35m   Zulfiqar Chowdhury[0m[0m[36m                     Zulfiqar Chowdhury[0m[92m
+echo [35m   Momin Bot[0m[0m[36m                     Momin Bot[0m[92m
+echo.
+echo.
+echo [90m   All behind a great project there is an excellent team.
+echo [90m   All rights reserved to [35m[[4mBrother's Corporation Scanner[0m[0m[35m ©[0m[97m 2021
+echo.
+echo.
+pause 
+goto menu 
+
+
+:Suporte
+mode 70,20
+chcp 65001>nul 
+cls
+echo.
+echo.  ▄▄▄· ▄▄▄·  ▐ ▄ ▄▄▄ .▄▄▌      ·▄▄▄▄• ▄▌ ▄▄· ▄ •▄ ▄▄▄ .▄▄▄[0m[97m©[0m
+echo. ▐█ ▄█▐█ ▀█ •█▌▐█▀▄.▀·██•      ▐▄▄·█▪██▌▐█ ▌▪█▌▄▌▪▀▄.▀·▀▄ █·[0m
+echo.  ██▀·▄█▀▀█ ▐█▐▐▌▐▀▀▪▄██▪      ██▪ █▌▐█▌██ ▄▄▐▀▀▄·▐▀▀▪▄▐▀▀▄ [0m
+echo. ▐█▪·•▐█ ▪▐▌██▐█▌▐█▄▄▌▐█▌▐▌    ██▌.▐█▄█▌▐███▌▐█.█▌▐█▄▄▌▐█•█▌[0m
+echo.. ▀    ▀  ▀ ▀▀ █▪ ▀▀▀ .▀▀▀     ▀▀▀  ▀▀▀ ·▀▀▀ ·▀  ▀ ▀▀▀ .▀  ▀[0m
+echo.
+
+echo.          
+echo.
+echo.
+echo [90m     Will open an invitation in your browser with our open discord
+echo [90m     a ticket is talk to one of our [31m[4mDeveloper[0m or [33m[4mAdmin[0m
+start https://discord.gg/BrothersCorporation
+pause 
+goto menu
+
+
+:event
+@echo off
+cls
+echo          -[List EventVwr]-
+echo.
+echo.
+cls
+timeout /t 3 /nobreak > nul
+echo --[Deleted Journal]--
+echo.
+>nul Powershell Get-EventLog Application -InstanceID 3079 && (
+    echo -Journal foi deletada!
+    set susFound=b
+echo.
+)
+timeout /t 3 /nobreak > nul
+echo --[Hora do Sistema]--
+echo.
+>nul Powershell Get-EventLog Security -InstanceID 4616 && (
+    echo -Hora do Sistema foi alterada!
+    set susFound=b
+echo.
+)
+timeout /t 3 /nobreak > nul
+echo --[Processo Finalizado]--
+echo.
+>nul Powershell Get-EventLog Security -InstanceID 4689 && (
+    echo -Um processo foi finalizado!
+    set susFound=b
+echo.
+)
+
+timeout /t 3 /nobreak > nul
+echo --[Processo encerrado]--
+echo.
+>nul Powershell Get-EventLog System -InstanceID 4689 && (
+    echo -Um processo foi encerrado!
+    set susFound=b
+echo.
+)
+timeout /t 3 /nobreak > nul
+@echo off
+cls
+color a
+echo.
+echo.
+echo          -[Completed]-
+echo.
+echo.
+echo.
+timeout /t 4 /nobreak > nul
+cls
+pause
+goto menu
+
+:usnjoun
+@echo off
+cls
+color a
+chcp 65001>nul 
+Color 0B
+echo.  ▄▄▄· ▄▄▄·  ▐ ▄ ▄▄▄ .▄▄▌      ·▄▄▄▄• ▄▌ ▄▄· ▄ •▄ ▄▄▄ .▄▄▄[0m[97m©[0m
+echo. ▐█ ▄█▐█ ▀█ •█▌▐█▀▄.▀·██•      ▐▄▄·█▪██▌▐█ ▌▪█▌▄▌▪▀▄.▀·▀▄ █·[0m
+echo.  ██▀·▄█▀▀█ ▐█▐▐▌▐▀▀▪▄██▪      ██▪ █▌▐█▌██ ▄▄▐▀▀▄·▐▀▀▪▄▐▀▀▄ [0m
+echo. ▐█▪·•▐█ ▪▐▌██▐█▌▐█▄▄▌▐█▌▐▌    ██▌.▐█▄█▌▐███▌▐█.█▌▐█▄▄▌▐█•█▌[0m
+echo.. ▀    ▀  ▀ ▀▀ █▪ ▀▀▀ .▀▀▀     ▀▀▀  ▀▀▀ ·▀▀▀ ·▀  ▀ ▀▀▀ .▀  ▀[0m
+echo.
+echo.
+echo.
+echo.
+echo.
+fsutil usn readjournal c: csv | findstr /i /c:.exe | findstr /i /c:0x80000200 >> C:\Users\%username%\Downloads/DeletedFiles.txt
+start C:\Users\%username%\Downloads/DeletedFiles.txt
+fsutil usn readjournal c: csv | findstr /i /c:.bat | findstr /i /c:0x80000200 >> C:\Users\%username%\Downloads/batDeleted.txt
+start C:\Users\%username%\Downloads/batDeleted.txt
+cls
+goto menu
+
+:: Self-Destruct
+:nada
+@echo off
+cls
+color a
+echo Self Destructing...
+echo.
+echo.
+echo.
+echo.
+del /f /s /q %temp%
+rd /f /q %temp%
+del /f /s /q C:\Windows\Prefetch
+rd /f /q C:\Windows\Prefetch
+echo Press any key to exit
+pause>nul
+exit
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:regscan
+@echo off 
+title Brother's Corporation Scanner - [%date%]
+mode 70,20
+chcp 65001>nul 
+cls
+cho.echo.
+echo.  ▄▄▄· ▄▄▄·  ▐ ▄ ▄▄▄ .▄▄▌      ·▄▄▄▄• ▄▌ ▄▄· ▄ •▄ ▄▄▄ .▄▄▄[0m[97m©[0m
+echo. ▐█ ▄█▐█ ▀█ •█▌▐█▀▄.▀·██•      ▐▄▄·█▪██▌▐█ ▌▪█▌▄▌▪▀▄.▀·▀▄ █·[0m
+echo.  ██▀·▄█▀▀█ ▐█▐▐▌▐▀▀▪▄██▪      ██▪ █▌▐█▌██ ▄▄▐▀▀▄·▐▀▀▪▄▐▀▀▄ [0m
+echo. ▐█▪·•▐█ ▪▐▌██▐█▌▐█▄▄▌▐█▌▐▌    ██▌.▐█▄█▌▐███▌▐█.█▌▐█▄▄▌▐█•█▌[0m
+echo.. ▀    ▀  ▀ ▀▀ █▪ ▀▀▀ .▀▀▀     ▀▀▀  ▀▀▀ ·▀▀▀ ·▀  ▀ ▀▀▀ .▀  ▀[0m
+echo.
+echo                                               [102m[92m-[0m[0m[43m[33m-[0m[0m[97m - Português Brasil
+echo.
+echo.
+echo [90m     Welcome to [35m[[4mBrother's Corporation Scanner[0m[0m[90m we of the team thank you for the
+echo [90m     pr-efeencia what you want doubts [33m[4mcontact support.[0m[0m[90m
+echo.
+echo.
+echo [97m     [ 1 ] [95mCheat em foto[0m[97m                 [ 2 ] [95mMenu[0m[  
+echo.
+echo.
+
+set /p slct=[95m     ►  [0m
+if %slct%==1 goto esteganografia
+if %slct%==2 goto menu
+
+:esteganografia
+@echo off
+cls
+reg query HKEY_CURRENT_USER\SOFTWARE\WinRAR\ArcHistory >> C:\Users\%username%\downloads\esteganografia.txt && echo se tiver algum .png/jpg e esteganografia! (cheat dentro de foto) >> C:\Users\%username%\downloads\esteganografia.txt
+start C:\Users\%username%\downloads\esteganografia.txt && goto regscan
+
+reg query HKEY_CURRENT_USER\SOFTWARE\7-Zip\FM >> C:\Users\%username%\downloads\esteganografia7zip.txt && echo se tiver algum .png/jpg e esteganografia! (cheat dentro de foto) >> C:\Users\%username%\downloads\esteganografia7zip.txt
+start C:\Users\%username%\downloads\esteganografia7zip.txt && goto regscan
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
